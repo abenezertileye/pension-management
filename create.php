6861f21351c53bd9890b87dd9bb0af975011d3a3
abene <abenezertileye2003@gmail.com>
2025-04-17 09:08:10 +0300

    create user modified

--- create.php
+++ create.php
@@ -5,6 +5,7 @@ error_reporting(1);
 error_reporting(-1);
 ini_set('display_errors', 'On');
 set_error_handler("var_dump");
+
 if (!isset($_SESSION['user'])) {
 	header("Location:login.php");
 } else {
@@ -14,40 +15,48 @@ if (!isset($_SESSION['user'])) {
 	}
 }
 
-$full_err = $role_err = $errmsg = "";
+$full_err = $role_err = $pass_err = $errmsg = "";
 if (isset($_POST['submit'])) {
 	if (empty(trim($_POST["fullname"]))) {
 		$full_err = "Please enter full name";
 	} else {
-		$full = $_POST["fullname"];
+		$full = trim($_POST["fullname"]);
 	}
 	if (empty(trim($_POST["role"]))) {
 		$role_err = "Please choose the role";
 	} else {
-		$role = $_POST["role"];
+		$role = trim($_POST["role"]);
+	}
+	if (empty(trim($_POST["password"]))) {
+		$pass_err = "Please enter a password";
+	} elseif (strlen(trim($_POST["password"])) < 8) {
+		$pass_err = "Password must be at least 8 characters long";
+	} else {
+		$pass = trim($_POST["password"]);
 	}
 
-	$sqlfull = "select * from users where username='$full' ";
-	$resultfull = $mysqli->query($sqlfull) or die($mysqli->error);
-	if ($resultfull->num_rows == 1) {
-		$errmsg = "This user is already created";
-	} else if (empty($full_err) && empty($role_err)) {
-		$pass = rand(12345678, 87654321);
-		echo $pass;
-		//$pass=password_hash($pass, PASSWORD_DEFAULT);
-		$sqlcreate = "insert into users(username,password,role,status) values('$full','$pass','$role',0)";
-		$resultcreate = $mysqli->query($sqlcreate) or die($mysqli->error);
-		$errmsg = "Account created successfully";
-		// mail("selamubachore@gmail.com","Your default password",$pass);
+	// Skip the check for existing users to allow duplicate usernames
+	if (empty($full_err) && empty($role_err) && empty($pass_err)) {
+		$sqlcreate = "INSERT INTO users (username, password, role, status) VALUES (?, ?, ?, 0)";
+		if ($stmt_create = $mysqli->prepare($sqlcreate)) {
+			$stmt_create->bind_param("sss", $full, $pass, $role);
+			if ($stmt_create->execute()) {
+				$errmsg = "Account created successfully";
+			} else {
+				$errmsg = "Error creating account: " . $mysqli->error;
+			}
+			$stmt_create->close();
+		} else {
+			$errmsg = "Database error: " . $mysqli->error;
+		}
 	}
 }
-
 ?>
 
 <html>
 
 <head>
-	<title>Account creation </title>
+	<title>Account creation</title>
 	<style>
 		a:hover {
 			color: black;
@@ -55,72 +64,178 @@ if (isset($_POST['submit'])) {
 		}
 
 		a {
-
 			text-decoration: none;
+		}
+
+		body {
+			background-color: lightblue;
+			margin: 0;
+		}
+
+		.header {
+			width: 100%;
+			background-color: gray;
+			margin-bottom: 10px;
+		}
+
+		.header img {
+			width: 100%;
+			display: block;
+		}
+
+		.sidebar {
+			padding: 20px;
+			width: 20%;
+			background-color: #E1F8DC;
+			text-align: center;
+			font-family: verdana;
+			font-size: 18px;
+			margin-top: 50px;
+			border-radius: 0;
+			float: left;
+			line-height: 50px;
+			border: solid blue 1px;
+			min-height: calc(100vh - 70px);
+		}
+
+		.sidebar a {
+			float: left;
+			color: blue;
+		}
+
+		.content {
+			float: right;
+			width: 70%;
+			padding: 10px;
+			margin-top: 50px;
+		}
+
+		.form-container {
+			background-color: white;
+			width: 100%;
+			margin: auto;
+			font-family: verdana;
+			font-size: 23px;
+			border-radius: 15px;
+			padding: 20px;
+			line-height: 45px;
+		}
+
+		.form-container input,
+		.form-container select {
+			width: 70%;
+			height: 30px;
+			font-size: 23px;
+		}
+
+		.form-container input[type="submit"] {
+			width: 50%;
+			height: 30px;
+			font-size: 23px;
+		}
+
+		.error {
+			color: red;
+			border: solid blue 1px;
+			padding: 5px;
+		}
+
+		.password-container {
+			position: relative;
+			width: 70%;
+		}
+
+		.password-container input {
+			width: 100%;
+			height: 30px;
+			font-size: 23px;
+		}
+
+		.password-toggle {
+			position: absolute;
+			right: 10px;
+			top: 50%;
+			transform: translateY(-50%);
+			cursor: pointer;
+			font-size: 20px;
 		}
 	</style>
 </head>
 
-<body bgcolor="lightblue">
-	<div style="width:100%;background-color:gray;margin-bottom:10px;">
+<body>
+	<div class="header">
 		<img src="pssa.jpg" style="width:100%;">
 	</div>
 	<?php
 	$user = $_SESSION['user'];
-	$sql = "select role from users where username='$user'";
-	$result = $mysqli->query($sql);
+	$sql = "SELECT role FROM users WHERE username = ?";
+	$stmt = $mysqli->prepare($sql);
+	$stmt->bind_param("s", $user);
+	$stmt->execute();
+	$result = $stmt->get_result();
 	$roles = $result->fetch_assoc();
+	$stmt->close();
 	?>
-	<div style="padding:20px;width:20%;margin:auto;background-color:E1F8DC;text-align:center;font-family:verdana;font-size:18px;
-             margin-top:50px; border-radius:0px;float:left;line-height:50px;border:solid blue 1px;height:100%;">
+	<div class="sidebar">
 		<?php if ($roles['role'] == "Admin") { ?>
-			<a href="register_company.php" style="float:left;">Register new company</a> <br>
-
-			<a href="choose_company.php" style="float:left;">Register new pensioner</a> <br>
-			<a href="choose_company_2.php" style="float:left;">Register Beneficiery</a> <br>
-			<a href="create.php" style="float:left;">Add new user</a><br>
-			<a href="report.php" style="float:left;">Generate report</a><br>
-			<a href="viewfeed.php" style="float:left;">View feedbacks</a><br>
-			<a href="logout.php" style="float:left;">Logout</a><br>
+			<a href="register_company.php">Register new company</a><br>
+			<a href="choose_company.php">Register new pensioner</a><br>
+			<a href="choose_company_2.php">Register Beneficiary</a><br>
+			<a href="create.php">Add new user</a><br>
+			<a href="report.php">Generate report</a><br>
+			<a href="viewfeed.php">View feedbacks</a><br>
+			<a href="logout.php">Logout</a><br>
 		<?php } else if ($roles['role'] == "Pensioner") { ?>
-			<a href="feedback.php" style="float:left;">Send feedback</a> <br>
-			<a href="report.php" style="float:left;">Generate report</a><br>
-			<a href="logout.php" style="float:left;">Logout</a><br>
-		<?php } else if ($roles['role'] == "Clerk") {	 ?>
-			<a href="choose_company.php" style="float:left;">Register new pensioner</a> <br>
-			<a href="report.php" style="float:left;">Generate report</a><br>
-			<a href="logout.php" style="float:left;">Logout</a><br>
-		<?php } else if ($roles['role'] == "Organization") {	 ?>
-			<a href="choose_company.php" style="float:left;">Register new pensioner</a> <br>
-			<a href="report.php" style="float:left;">Generate report</a><br>
-			<a href="logout.php" style="float:left;">Logout</a><br>
+			<a href="feedback.php">Send feedback</a><br>
+			<a href="report.php">Generate report</a><br>
+			<a href="logout.php">Logout</a><br>
+		<?php } else if ($roles['role'] == "Clerk") { ?>
+			<a href="choose_company.php">Register new pensioner</a><br>
+			<a href="report.php">Generate report</a><br>
+			<a href="logout.php">Logout</a><br>
+		<?php } else if ($roles['role'] == "Organization") { ?>
+			<a href="choose_company.php">Register new pensioner</a><br>
+			<a href="report.php">Generate report</a><br>
+			<a href="logout.php">Logout</a><br>
 		<?php } ?>
-
 	</div>
-	<div style="float:right;width:70%;margin:auto;">
-
-		<form method="post" enctype="multipart/form-data" action="create.php"
-			style="background-color:white;font-family:verdana;font-size:23px;border-radius:15px;padding:20px;line-height:45px;margin-top:50px;">
-			<p style="color:red;border:solid blue 1px;">
-			<h3 style="font-family:verdana;font-size:23px;">Create account</h3>
-			<?php
-			echo "<font color=red>" . $errmsg . " " . $full_err . " " . $role_err . "</font>";
-			?>
+	<div class="content">
+		<form method="post" enctype="multipart/form-data" action="create.php" class="form-container">
+			<p class="error">
+			<h3>Create account</h3>
+			<?php echo htmlspecialchars($errmsg . " " . $full_err . " " . $role_err . " " . $pass_err); ?>
 			</p>
 			<hr>
 			<br>
 			<label for="fullname">Fullname</label><br>
-			<input name="fullname" type="text" style="width:70%;height:30;"><br>
+			<input name="fullname" type="text"><br>
 			<label for="role">Role</label><br>
-			<select name="role" style="width:70%;height:30;font-size:23px;">
-				<option>Admin</option>
-				<option>Pensioner</option>
-				<option>Clerk</option>
-				<option>Organization</option>
-			</select><br><br>
-			<input name="submit" type="submit" value="Create" style="width:50%;height:30;font-size:23px;">
+			<select name="role">
+				<option value="">Select Role</option>
+				<option value="Admin">Admin</option>
+				<option value="Clerk">Clerk</option>
+			</select><br>
+			<label for="password">Password</label><br>
+			<div class="password-container">
+				<input type="password" id="password" name="password" minlength="8">
+				<span class="password-toggle" onclick="togglePassword()">👁️</span>
+			</div><br><br>
+			<input name="submit" type="submit" value="Create">
 		</form>
 	</div>
+	<script>
+		function togglePassword() {
+			const passwordInput = document.getElementById('password');
+			const toggleButton = document.querySelector('.password-toggle');
+			if (passwordInput.type === 'password') {
+				passwordInput.type = 'text';
+				toggleButton.textContent = '🙈';
+			} else {
+				passwordInput.type = 'password';
+				toggleButton.textContent = '👁️';
+			}
+		}
+	</script>
 </body>
 
 </html>
